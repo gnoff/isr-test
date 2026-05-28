6a9b7d777f04c9606ad453dd5d2eaf43465d284d
Josh Story <gnoff@storyposted.com>
2026-05-28 16:50:21 -0700

    Remove RenderInfo to improve UI transitions
    this was hacky but it interfered with normal UI transitions

--- app/ppr-isr/[slug]/page.tsx
+++ app/ppr-isr/[slug]/page.tsx
@@ -3,7 +3,6 @@ import { Suspense } from "react";
 import { sleep } from "../../components/sleep";
 import { RenderInfo, getRenderInfoKey } from "../../components/render-info";
 
-
 export async function generateStaticParams() {
   return [{ slug: "a" }];
 }
@@ -48,17 +47,28 @@ export default async function SlugPage({
         <DynamicContent />
       </Suspense>
 
-      <RenderInfo now={await getRenderInfoKey()} />
+      {/*<RenderInfo now={await getRenderInfoKey()} />*/}
 
       <section>
         <h2>Navigate</h2>
-        <p>Each link appends a character to the current slug, creating a new uncached page.</p>
+        <p>
+          Each link appends a character to the current slug, creating a new
+          uncached page.
+        </p>
         <ul>
-          <li><Link href={`/ppr-isr/${slug}a`}>{slug}a</Link></li>
-          <li><Link href={`/ppr-isr/${slug}b`}>{slug}b</Link></li>
-          <li><Link href={`/ppr-isr/${slug}c`}>{slug}c</Link></li>
+          <li>
+            <Link href={`/ppr-isr/${slug}a`}>{slug}a</Link>
+          </li>
+          <li>
+            <Link href={`/ppr-isr/${slug}b`}>{slug}b</Link>
+          </li>
+          <li>
+            <Link href={`/ppr-isr/${slug}c`}>{slug}c</Link>
+          </li>
         </ul>
-        <p><Link href="/ppr-isr">Home</Link></p>
+        <p>
+          <Link href="/ppr-isr">Home</Link>
+        </p>
       </section>
     </main>
   );
